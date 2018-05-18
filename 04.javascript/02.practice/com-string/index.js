efc7b21e5c52bf8b8f5b0e3af1fb6b7a6dc5f0d7
Tu Nguyen <nvminhtu@gmail.com>
2018-05-18 22:07:27 +0700

    javascript with regular expression

--- 04.javascript/02.practice/com-string/index.js
+++ 04.javascript/02.practice/com-string/index.js
@@ -23,3 +23,16 @@ console.log(removeEnd);
 /// or use substr - can use length for slicing
 var removeEndEle = fruits.substr(1,4); // from, length to get
 console.log(removeEndEle);
+
+/// repalce string expression regular
+var welTech = "This is a product of Google, a year later, Facebook also buy Instagram";
+document.getElementById('welTech').innerHTML = welTech;
+
+function changeText() {
+    var str = document.getElementById('welTech').innerHTML;
+    //var txt = str.replace('GOOGLE','Facebook'); // will have error
+   
+    var txt = str.replace('/Google/i','Facebook');
+    console.log(txt);
+    str.innerHTML = txt;
+}
