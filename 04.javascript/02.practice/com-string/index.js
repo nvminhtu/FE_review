0a8217edfd176ed2fe679c8bff4c443fea2d742b
Tu Nguyen <nvminhtu@gmail.com>
2018-05-19 22:34:28 +0700

    add project countdown - pure javascript

--- 04.javascript/02.practice/com-string/index.js
+++ 04.javascript/02.practice/com-string/index.js
@@ -25,8 +25,9 @@ var removeEndEle = fruits.substr(1,4); // from, length to get
 console.log(removeEndEle);
 
 /// repalce string expression regular
-var welTech = "This is a product of Google, a year later, Facebook also buy Instagram";
-document.getElementById('welTech').innerHTML = welTech;
+// var welTech = "This is a product of Google, a year later, Facebook also buy Instagram";
+// document.getElementById('welTech').innerHTML = welTech;
+
 
 function changeText() {
     var str = document.getElementById('welTech').innerHTML;
@@ -36,3 +37,34 @@ function changeText() {
     console.log(txt);
     str.innerHTML = txt;
 }
+
+// convert string
+var normalText = "hello, i am Tu";
+var text2 = normalText.toUpperCase();
+console.log(text2);
+
+// concat: nối chuỗi
+var txt1 = "TU";
+var txt2 = "NGUYEN";
+var fullname = txt1.concat(" ", txt2);
+console.log(fullname);
+
+// lấy từ đầu tiên trong chuỗi (vd: url) (trả về dạng unicode)
+var txt="m300";
+var getFirst = txt.charCodeAt(0);
+console.log(getFirst);
+
+// string - we can get first element easy
+var stringFull = "Minh Tu";
+var firstChar = stringFull[0];
+console.log(firstChar);
+
+// convert string to an array
+var stringList = "A, B, C, D, E";
+var arr = stringList.split(",");
+console.log(arr);
+
+// convert to string 
+x = 100;
+var y = x.toString();
+console.log(y);
